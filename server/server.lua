b3f4e91753a4912fcbb580c9941bac443399f5e1
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 13:30:09 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,11 +1,39 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
-
 local horsename = nil
 local horsexp = 0
 local newxp = 0
 local horseid = nil
 
------------------------------------------------------------------------------------
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-horsetrainer/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
 
 -- use horse trainer brush
 RSGCore.Functions.CreateUseableItem("horsetrainingbrush", function(source, item)
@@ -97,4 +125,8 @@ RegisterNetEvent('rsg-horsetrainer:server:deleteItem', function(item, amount)
     TriggerClientEvent("inventory:client:ItemBox", src, RSGCore.Shared.Items[item], "remove")
 end)
 
------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
+
